5e4b47f982767942f4a4d8136e42293f23ee4d5b
HigorSkw <higor.skowronski@gmail.com>
2023-02-24 11:20:49 -0300

    fix: routes bug

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,19 +1,17 @@
-import { Route, Routes, BrowserRouter, useNavigate } from "react-router-dom";
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
 
 import HomePage from "../Pages/Home";
 import LoginPage from "../Pages/Login";
 import RegisterPage from "../Pages/Register";
 
 export const RoutesMain = () => {
-
   return (
-    <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<HomePage/>} />
+    <Routes>
+      <Route path="/*" element={<Navigate replace to={"home"} />} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
+    </Routes>
   );
 };
