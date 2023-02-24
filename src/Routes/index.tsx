a30da1be6b35b11fbbc7eaa96ebda12326abeda6
Erica Lopes <ericalopes@outlook.com>
2023-02-24 12:35:03 -0300

    feat: add component home + fix menu icon navbar

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,19 +1,18 @@
-import { Route, Routes, BrowserRouter, useNavigate } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import HomePage from "../Pages/Home";
 import LoginPage from "../Pages/Login";
+import ProfileViewAdmin from "../Pages/ProfileViewAdmin";
 import RegisterPage from "../Pages/Register";
 
 export const RoutesMain = () => {
-
   return (
-    <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<HomePage/>} />
+    <Routes>
+      <Route path="/*" element={<Navigate replace to={"home"} />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
+      <Route path="/profile" element={<ProfileViewAdmin />} />
+    </Routes>
   );
 };
